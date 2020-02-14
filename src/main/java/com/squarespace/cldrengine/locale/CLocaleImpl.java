d208638d89e7e8068239ab95953b3db110383946
Patrick Hensley <pathensley@gmail.com>
2020-02-14 17:23:27 -0500

    Hide class from coverage

--- src/main/java/com/squarespace/cldrengine/locale/CLocaleImpl.java
+++ src/main/java/com/squarespace/cldrengine/locale/CLocaleImpl.java
@@ -3,20 +3,19 @@ package com.squarespace.cldrengine.locale;
 import com.squarespace.cldrengine.api.CLocale;
 import com.squarespace.cldrengine.api.LanguageTag;
 
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Generated;
 import lombok.ToString;
 
+@Generated
+@AllArgsConstructor
 @EqualsAndHashCode
 @ToString
 public class CLocaleImpl implements CLocale {
 
   private final String id;
   private final LanguageTag tag;
-
-  public CLocaleImpl(String id, LanguageTag tag) {
-    this.id = id;
-    this.tag = tag;
-  }
 
   @Override
   public String id() {

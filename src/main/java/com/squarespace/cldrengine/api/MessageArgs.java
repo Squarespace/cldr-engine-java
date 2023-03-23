e07c04103096311245ffb389b4f9e5ca1979e7cb
Patrick Hensley <pathensley@gmail.com>
2023-03-23 16:07:10 -0400

    Javadocs for MessageArgs, remove confusing build method

--- src/main/java/com/squarespace/cldrengine/api/MessageArgs.java
+++ src/main/java/com/squarespace/cldrengine/api/MessageArgs.java
@@ -10,24 +10,40 @@ public class MessageArgs {
   private final List<Object> positional = new ArrayList<>();
   private final Map<String, Object> named = new HashMap<>();
 
-  public static MessageArgs build() {
-    return new MessageArgs();
+  /**
+   * Arguments can be a mix of named and positional, so the count is
+   * the sum of both types.
+   */
+  public int count() {
+    return positional.size() + named.size();
   }
 
+  /**
+   * Add a positional argument.
+   */
   public MessageArgs add(Object arg) {
     this.positional.add(arg);
     return this;
   }
 
+  /**
+   * Add a named argument.
+   */
   public MessageArgs add(String name, Object arg) {
     this.named.put(name, arg);
     return this;
   }
 
+  /**
+   * Get a positional argument.
+   */
   public Object get(int i) {
     return i < positional.size() ? positional.get(i) : null;
   }
 
+  /**
+   * Get a named argument.
+   */
   public Object get(String name) {
     return named.get(name);
   }
